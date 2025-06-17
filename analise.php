fe715801b9164ce19f1d5695e3d834df65be2a4c
Jean Thielis <jeanthielis@gmail.com>
2025-06-17 18:02:04 -0300

    11062025

--- analise.php
+++ analise.php
@@ -4,12 +4,12 @@ $sql2="SELECT sum(valor) as soma,count(id_agenda) as festa,sum(entrada) as entra
 include_once('conexao.php');
 $resultadoSoma = mysqli_query($conn,$sql2);
 while($result = mysqli_fetch_assoc($resultadoSoma)){
-    $soma = "R$ ".number_format($result['soma'],2,',','.');
+    $soma = number_format($result['soma'],2,',','.');
     $festa = $result['festa'];
-    $entrada = "R$ ".number_format($result['entrada'],2,',','.');
-    $lucro = "R$ ".number_format($result['lucro'],2,',','.');
-    $media = "R$ ".number_format($result['media'],2,',','.');
-    $lucroMensal = "R$ ".number_format($result['lucroMensal'],2,',','.');
+    $entrada = number_format($result['entrada'],2,',','.');
+    $lucro = number_format($result['lucro'],2,',','.');
+    $media = number_format($result['media'],2,',','.');
+    $lucroMensal = number_format($result['lucroMensal'],2,',','.');
 }
 ?>
 <html>
@@ -17,18 +17,17 @@ while($result = mysqli_fetch_assoc($resultadoSoma)){
     <br>
     <hr>
     <div class="row">
-        <div class="col text-center p-0 m-2">
-            <div class="alert bg-primary">
-                
-                <strong class='text-white'><?php echo $soma?></strong><br>
-                <strong class='text-white' for="">Faturamento Anual</strong>
+        <div class="col-md-6 mb-3">
+            <div class="alert bordaAnalise">
+                <strong class='text-secondary' for="">Faturamento Anual</strong>
+                <strong class='text-secondary'><h1><?php echo $soma?></strong></h1><br>
             </div>
             
         </div>
-        <div class="col text-center p-0 m-2 ">
-            <div class="alert bg-success ">
-                <strong class='text-white'><?php echo $festa?></strong><br>
-                <strong class='text-white' for="">Festa Concluidas</strong>
+        <div class="col-md-6 mb-3">
+            <div class="alert bordaAnalise">
+                <strong class='text-secondary' for="">Festa Concluidas</strong>
+                <strong class='text-secondary'><h1><?php echo $festa?></strong></h1><br>
             </div>
             
         </div>
@@ -36,34 +35,37 @@ while($result = mysqli_fetch_assoc($resultadoSoma)){
 
 
     <div class="row">
-        <div class="col text-center p-0 m-2">
-            <div class="alert bg-warning">
-                <strong class='text-white'><?php echo $entrada?></strong><br>
-                <strong class='text-white' for="">Entrada</strong>
+        <div class="col-sm">
+
+            <div class="alert bordaAnalise">
+                <strong class='text-secondary' for="">Entrada</strong><br>
+                <strong class='text-secondary'><h1><?php echo $entrada?></strong></h1><br>
             </div>
             
         </div>
-        <div class="col text-center p-0 m-2">
-            <div class="alert bg-danger">
-                <strong class='text-white'><?php echo $lucro?></strong><br>
-                <strong class='text-white' for="">Lucro Anual</strong>
+        <div class="col-sm">
+            <div class="alert bordaAnalise">
+                 <strong class='text-secondary' for="">Lucro Anual</strong><br>
+                <strong class='text-secondary'><h1><?php echo $lucro?></strong></h1><br>
+               
             </div>    
         </div>
     </div>
 
 
     <div class="row">
-        <div class="col text-center p-0 m-2">
-            <div class="alert bg-info">
-                <strong class='text-white'><?php echo $media?></strong><br>
-                <strong class='text-white' for="">Média Mensal</strong>
+        <div class="col-sm">
+            <div class="alert bordaAnalise">
+                <strong class='text-secondary' for="">Média Mensal</strong>
+                <strong class='text-secondary'><h1><?php echo $media?></strong></h1><br>
             </div>
             
         </div>
-        <div class="col text-center p-0 m-2">
-        <div class="alert bg-dark">
-                <strong class='text-white'><?php echo $lucroMensal?></strong><br>
-                <strong class='text-white' for="">Lucro Mensal</strong>
+        <div class="col-sm">
+        <div class="alert bordaAnalise">
+            <strong class='text-secondary' for="">Lucro Mensal</strong>
+            <strong class='text-secondary'><h1><?php echo $lucroMensal?></strong></h1><br>
+                
             </div>
             
         </div>
